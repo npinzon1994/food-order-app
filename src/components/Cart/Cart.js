35fda61b1e2fd72d40f321ae57a7f1f9ad8c006d
Nikki Pinzon <npinzon1994@gmail.com>
2022-11-18 13:48:46 -0500

    totalPrice is coming up as undefined. Need to figure out why.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,24 +1,36 @@
-import React, { useContext} from "react";
+import React, { useContext } from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
 import CartContext from "../../context/cart-context";
+import CartItem from "../Cart/CartItem";
 
 const Cart = (props) => {
   const cartContext = useContext(CartContext);
 
+  const cartItems = cartContext.items.map((item) => (
+    <CartItem
+      id={item.id}
+      name={item.name}
+      price={item.price}
+      quantity={item.quantity}
+    />
+  ));
+
+  const totalPrice = `$${cartContext.totalPrice}`;
+  const hasItems = cartContext.items.length > 0;
+
   return (
     <Modal onClose={props.onClose}>
-      <ul className={classes["cart-items"]}></ul>
-
+      <ul className={classes["cart-items"]}>{cartItems}</ul>
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>{`$${cartContext.totalQuantity}`}</span>
+        <span>{totalPrice}</span>
       </div>
       <div className={classes.actions}>
         <button onClick={props.onClose} className={classes["button--alt"]}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
