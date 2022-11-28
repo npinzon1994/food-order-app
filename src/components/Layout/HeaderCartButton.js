e670de1ca1ec3ca61568cfe05110cfb2d1c1ea9b
Nikki Pinzon <npinzon1994@gmail.com>
2022-11-28 11:10:37 -0500

    added effect for cart button animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,9 +1,10 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../../context/cart-context";
 import CartIcon from "../../images/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
   const cartContext = useContext(CartContext);
 
   /*reduce method allows us to transform an array of data 
@@ -17,12 +18,38 @@ const HeaderCartButton = (props) => {
   simply add that number to our currentNum, which is going to be
   the totalQuantity we use in the HeaderCartButton.
   */
-  const numberOfCartItems = cartContext.items.reduce((currentNum, item) => {
+
+  //ig items is the alias name as well as the name of the prop in cartContext
+  //we're PULLING items out of cartContext so we can use it as 'items'
+  const { items } = cartContext;
+
+  const numberOfCartItems = items.reduce((currentNum, item) => {
     return currentNum + item.quantity;
   }, 0);
 
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+
+    setButtonIsHighlighted(true);
+    
+    //waits 300ms then removes highlighted class
+    const timer = setTimeout(() => {
+      setButtonIsHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items]);
+
+  const buttonClasses = `${classes.button} ${
+    buttonIsHighlighted ? classes.bump : ""
+  }`;
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    //add .bump class only when # cart items changes
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
